aef3b6f89a203024c2c2e7306944971c9a8c3c7a
Amy Shafer <amy.u.shafer@gmail.com>
2022-01-09 02:58:07 -0500

    Stuck on saving to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -2,35 +2,34 @@ var now = moment().format('MMMM Do YYYY');
 var timeHere = document.getElementById('currentDay');
 timeHere.textContent = now;
 var timeBlocks = document.getElementsByClassName('time-block');
+var saveToDo = document.querySelectorAll('description');
 
 /* GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+THEN the current day is displayed at the top of the calendar (CHECK)
+WHEN I scroll down (CHECK)
+THEN I am presented with timeblocks for standard business hours (CHECK)
+WHEN I view the timeblocks for that day (CHECK)
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future (CHECK)
 WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
+THEN I can enter an event (CHECK)
+WHEN I click the save button for that timeblock 
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist
 */
 
-function timeClock() {
+function pastPresentFuture() {
   var presentHour = moment().hours(); // the block index that should be red
   var futureHours = 24 - presentHour; // how many blocks should be blacked out 
   var pastHours = 16 - presentHour; // how many blocks should brown
-  console.log("present hour: " + presentHour);
 
   $(".time-block").each(function(index) {
     // present time block should be black
-    if (index + 16 === presentHour) {
-      console.log("INDEX: " + index);
+    if (index === presentHour) {
       timeBlocks[index].setAttribute("style", "background-color: #030100;");
     // past time blocks should be greyed out
-    } else if (index + 16 < presentHour) {
+    } else if (index < presentHour) {
       timeBlocks[index].setAttribute("style", "background-color:#929292;");
     // future time blocks should be brown 
     } else {
@@ -39,7 +38,23 @@ function timeClock() {
   });
 } 
 
-timeClock();
+function userSaveEvent() {
+  var userToDo = document.getElementById('hour-4').value;
+  localStorage.setItem("Time", userToDo);
+  
+    // loop through the multiple choices
+    for (var i = 0; i < saveToDo.length; i++) {
+      var currentButton = saveToDo[i];
+      currentButton.addEventListener("click", function(event) {
+        var userSave = event.target.currentButton;
+        console.log(userSave);
+      });
+    }   
+}
+
+pastPresentFuture();
+saveToDo.addEventListener("click", userSaveEvent);
+
 
 /*
 
@@ -58,5 +73,14 @@ $(".time-block").each(function(index) {
     } else {
       timeBlocks[index].setAttribute("style", "background-color:#030100");
     }
+
+    for (var i = 0; i < timeBlocksArr.length; i++) {
+    var currentHour = timeBlocksArr[i];
+    currentHour.addEventListener("click", function(event) {
+      var userToDo = event.target.innerHTML;
+      console.log(userToDo);
+      localStorage.setItem("userAdd", userToDo);
+    });
+  }
 
 */
