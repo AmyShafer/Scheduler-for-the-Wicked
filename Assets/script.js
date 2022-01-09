d115e2cc4b52da582962dd256776255b723a895e
Amy Shafer <amy.u.shafer@gmail.com>
2022-01-08 23:52:09 -0500

    The time blocks are changing color as time passes. I changed the time block times just so I could keep working on it. There's probably an easier way to do that but I don't know what it is presently.

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 var now = moment().format('MMMM Do YYYY');
 var timeHere = document.getElementById('currentDay');
 timeHere.textContent = now;
+var timeBlocks = document.getElementsByClassName('time-block');
 
 /* GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
@@ -18,12 +19,24 @@ THEN the saved events persist
 */
 
 function timeClock() {
-  var pastHours = presentHour - 9;
-  var presentHour = moment().hours();
-  var futureHours = 17 - presentHour;
-  console.log("PAST: " + pastHours);
-  console.log("PRESENT: " + presentHour);
-  console.log("FUTURE: " + futureHours);
+  var presentHour = moment().hours(); // the block index that should be red
+  var futureHours = 24 - presentHour; // how many blocks should be blacked out 
+  var pastHours = 16 - presentHour; // how many blocks should brown
+  console.log("present hour: " + presentHour);
+
+  $(".time-block").each(function(index) {
+    // present time block should be black
+    if (index + 16 === presentHour) {
+      console.log("INDEX: " + index);
+      timeBlocks[index].setAttribute("style", "background-color: #030100;");
+    // past time blocks should be greyed out
+    } else if (index + 16 < presentHour) {
+      timeBlocks[index].setAttribute("style", "background-color:#929292;");
+    // future time blocks should be brown 
+    } else {
+      timeBlocks[index].setAttribute("style", "background-color:#52322c");
+    }
+  });
 } 
 
 timeClock();
@@ -37,5 +50,13 @@ $(".time-block").each(function(index) {
         parseInt($(this).attr("id").split("-")[1]);
       } 
     });
+
+     // past time blocks
+    if (currentHour > presentHour) {
+      timeBlocks[index].setAttribute("style", "background-color: #52322c");
+    // future time blocks  
+    } else {
+      timeBlocks[index].setAttribute("style", "background-color:#030100");
+    }
 
 */
