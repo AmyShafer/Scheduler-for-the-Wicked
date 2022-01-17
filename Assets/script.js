d366109e1fa451bac6de286c0fca2c31d9f949a6
Amy Shafer <amy.u.shafer@gmail.com>
2022-01-16 21:45:33 -0500

    progress on local storage but it is not there yet

--- Assets/script.js
+++ Assets/script.js
@@ -5,7 +5,11 @@ var timeBlocks = document.getElementsByClassName('time-block');
 var skullSave = $("saveBtn");
 var saveToDo = localStorage.getItem("todo");
 
-/* GIVEN I am using a daily planner to create a schedule
+var todos = [];
+
+/*
+
+GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
 THEN the current day is displayed at the top of the calendar (CHECK)
 WHEN I scroll down (CHECK)
@@ -14,14 +18,16 @@ WHEN I view the timeblocks for that day (CHECK)
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future (CHECK)
 WHEN I click into a timeblock (CHECK)
 THEN I can enter an event (CHECK)
-WHEN I click the save button for that timeblock 
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
+WHEN I click the save button for that timeblock (CHECK)
+THEN the text for that event is saved in local storage (CHECK)
+WHEN I refresh the page 
+THEN the saved events persist 
+
 */
 
+/* Function color codes time blocks for past, present, and future */ 
 function pastPresentFuture() {
-  var presentHour = moment().hours(); // the block index that should be red
+  var presentHour = moment().hours();
   
   $(".time-block").each(function(index) {
     var currentHour = index;
@@ -38,12 +44,34 @@ function pastPresentFuture() {
   });
 } 
 
+/* Function allows user to save their todos after closing/refreshing the page */
 function saveUserTodo(event) {
-  var saveClicked = $(event.currentTarget).prev().val();
-  var key = $(event.currentTarget).prev().data("set");
+  var saveClicked = $(event.currentTarget).prev().val(); // the todo text
+  var key = $(event.currentTarget).prev().data("set"); // the index of the time block
   localStorage.setItem(key, saveClicked);
+  //localStorage.setItem("todoList", todos);
+  var savedItems = localStorage.getItem(key);
+
+  todos.push(key, savedItems); 
+  localStorage.setItem("todoList", todos);
 }
 
+function displayToDos() {
+  var todoList = JSON.stringify(localStorage.getItem("todoList"));
+  console.log("todoList: " + todoList);
+ 
+  for (var i = 0; i < todoList.length; i++) {
+    var currentDataSet = todoList[i]; // data-set plus the number at that index
+    var todoItem = todoList[i + 1];
+    console.log("currentDataSet: " + currentDataSet);
+    console.log("todoItem: " + todoItem);
+    document.getElementById(currentDataSet).innerHTML = todoList[i + 1];
+    //console.log();
+  }  
+  
+}
+
+/* jQuery event listener - listens for the save buttons being clicked */
 $('.btn').each(function() {
   $(this).click(function(event) {
     saveUserTodo(event)
@@ -51,5 +79,5 @@ $('.btn').each(function() {
 })
 
 pastPresentFuture();
-//skullSave.on('click', '.saveBtn', saveUserTodo);
+displayToDos();
 
